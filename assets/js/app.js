d6d2e03998d50caca53f4cd480d9b78eddb4d864
Saagar <saagarchangoer@gmail.com>
2016-11-29 02:28:27 +0100

    Angular

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+var app = angular.module('appEditor', []);
+
+var obs = [{
+	title: 'Webdeveloper',
+	company: 'Mediabureau',
+	place: 'Almere',
+	dates: '25 januari tot 2 februari',
+	body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis officia fugit, assumenda ad soluta atque eum facilis incidunt esse. Mollitia ea,'
+},	{
+	title: 'Chauffeur',
+	company: 'TNT',
+	place: 'Amsterdam',
+	dates: '25 januari tot 2 februari',
+	body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+
+}
+]
+
+app.controller('MyController', function() {
+   this.jobs = obs;
+});
+
+
+
+
+
