25819f44027bd3e855a02091583b67082cdaefe0
Ergin Ulutas <ergin_u@hotmail.com>
2016-12-03 01:03:43 +0100

    added education section
    added education section and made most of the code modular for future
    sections.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,21 +2,38 @@
 var app = angular.module('appEditor', []);
 var $j = $.noConflict();
 
-var obs = [{
-	title: 'Webdeveloper',
-	company: 'Mediabureau',
-	place: 'Almere',
-	dates: ['25 januari 2016', '02 februari 2016', "Mo Jan 25 2016" , 'Tue Feb 2 2016'],
-	body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis officia fugit, assumenda ad soluta atque eum facilis incidunt esse. Mollitia ea,'
-},	{
-	title: 'Chauffeur',
-	company: 'TNT',
-	place: 'Amsterdam',
-	dates: ['25 januari 2016', '02 februari 2016', 'Mo Jan 25 2016' , 'Tue Feb 2 2016'],
-	body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+var obs = {
+	job: [{
+			job: 'Webdeveloper',
+			company: 'Mediabureau',			
+			place: 'Almere',			
+			body: 'LLorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis officia fugit, assumenda ad soluta atque eum facilis incidunt esse. Mollitia ea,',
+			dates: ['25 januari 2016', '02 februari 2016', "Mo Jan 25 2016" , 'Tue Feb 2 2016']
+		},	{
+			job: 'Chauffeur',
+			company: 'TNT',
+			place: 'Amsterdam',			
+			body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+			dates: ['26 januari 2016', '03 februari 2016', 'Tue Jan 26 2016' , 'Wed Feb 3 2016']
+		}
+	],
 
+	education: [{
+			education:"Webdeveloper",
+			schoolName:"ROC",
+			place:"Almere",
+			state:"finished",
+			dates:["27 januari 2016","04 februari 2016","Wed Jan 27 2016","Thu Feb 4 2016"],			
+		},{
+			education:"Comp Science",
+			schoolName:"HVA",
+			place:"Almere",
+			state:"finished",
+			dates:["25 januari 2016","02 februari 2016","Mo Jan 25 2016","Tue Feb 2 2016"],			
+		}
+	]
 }
-]
+
 //converts input date to a readable date string
 function dateConverter(date){
 	fixedDate = date.setHours(date.getHours() + 1);
@@ -26,78 +43,156 @@ function dateConverter(date){
 	return convertedDate.substring(8, 10) + " " + month + " " + convertedDate.substring(0, 4);
 }
 
+//returns children of a div as an array
+function returnChildren(object){
+	var childArr = [];
+	$j("#" + Object.keys(object[0])[0] + "-form").children().each(function(){
+		if($j(this).attr("id") != undefined && $j(this).attr("id").substring(0, 4) != "date"){
+			childArr.push($j(this).attr("id"))
+		}	
+	})
+	return childArr;
+}
+
+
 app.controller('MyController', ['$scope', function($scope) {
-	$scope.jobs = obs;
+
+	$scope.jobs = obs.job;
+	$scope.educations = obs.education;
 	$scope.editing = false;
 	$scope.editIndex = "";
-	$scope.dateTest = "";
 
 	//adds the content of edit-experience form to object or if editing is true replaces part of object
-	$scope.addJob = function(){
-		var fromDate = $j( "#dateFrom").datepicker( "getDate" );
-		var tillDate = $j( "#dateTill").datepicker( "getDate" );
+	$scope.addJob = function(object){
+		var fromDate = $j( "#dateFrom-" + Object.keys(object[0])[0]).datepicker( "getDate" );
+		var tillDate = $j( "#dateTill-" + Object.keys(object[0])[0]).datepicker( "getDate" );
+		var childArr = returnChildren(object);
+		console.log(childArr);
 
 		if ($scope.editing == false){
-			$scope.jobs.push({
-				'title':$scope.jobTitle, 
-				'company':$scope.jobCompany, 
-				'place':$scope.jobPlace, 
-				'dates':[dateConverter(fromDate), dateConverter(tillDate), fromDate.toDateString(), tillDate.toDateString()], 
-				'body':$scope.jobBody
-			})
+			object.push({});
+			var lastObject = object.length - 1;
+			console.log(lastObject)
+			childArr.forEach(function(value, index){
+				object[lastObject][Object.keys(object[0])[index]] = $j("#" + value).val(); 
+			});	
+			//This is going to cause problems for sections that don't have a date.			
+			object[lastObject].dates = [dateConverter(fromDate), dateConverter(tillDate), fromDate.toDateString(-1), tillDate.toDateString()]
 		} else {
-			$scope.jobs[$scope.editIndex] = {
-				'title':$scope.jobTitle, 
-				'company':$scope.jobCompany, 
-				'place':$scope.jobPlace, 
-				'dates':[dateConverter(fromDate), dateConverter(tillDate), fromDate.toDateString(-1), tillDate.toDateString()], 
-				'body':$scope.jobBody
-			}
+			childArr.forEach(function(value, index){
+				object[$scope.editIndex][Object.keys(object[0])[index]] = $j("#" + value).val(); 
+			});				
+			//This is going to cause problems for sections that don't have a date.			
+			object[$scope.editIndex].dates = [dateConverter(fromDate), dateConverter(tillDate), fromDate.toDateString(-1), tillDate.toDateString()]
 		}
 		$scope.editing = false;
 		$scope.editIndex = "";
-
 		$scope.clearInput();
+		console.log(JSON.stringify(object));
 	}	
 
 	//button for removing of the given section
-   $scope.removeJob = function(index){
-	   $scope.jobs.splice(index, index + 1)
-	   $scope.editing = false;
-	   $scope.editIndex = "";
-	   $scope.clearInput();
+   $scope.removeJob = function(index, object){
+   	console.log(index)
+		object.splice(index, 1)
+		$scope.editing = false;
+		$scope.editIndex = "";
+		$scope.clearInput();
+		console.log(object)
    }
 
+   
+
+
 	//button for editing a section
-	$scope.editButton = function(index){
-		 var fromDateObject = new Date($scope.jobs[index].dates[2]);
-		 var tillDateObject = new Date($scope.jobs[index].dates[3]);
+	$scope.editButton = function(index, object){
+		//first key in given object
+		var objectKey = Object.keys(object[index])[0];
+
+		//creates dates in the format datepicker needs
+		var fromDateObject = new Date(object[index].dates[2]);
+		var tillDateObject = new Date(object[index].dates[3]);
+
+		var childArr = returnChildren(object);
+
+		 //changes value of every input to the values of the edited object
+		childArr.forEach(function(value, arrayIndex){
+		 	var objectKey = Object.keys(object[index])[arrayIndex]
+			$j("#" + value).val(object[index][objectKey]);		 
+		})		 
+		 
 		$scope.editIndex = index;
 		$scope.editing = true;
-		$scope.jobTitle = $scope.jobs[index].title;
-		$scope.jobCompany = $scope.jobs[index].company;
-		$scope.jobPlace = $scope.jobs[index].place;		
-		$j("#dateFrom").datepicker( "setDate", fromDateObject);
-		$j("#dateTill").datepicker( "setDate", tillDateObject);
-		$scope.jobBody = $scope.jobs[index].body;
+		$j("#dateFrom-" + objectKey).datepicker( "setDate", fromDateObject);
+		$j("#dateTill-" + objectKey).datepicker( "setDate", tillDateObject);
 	}
 
 	//clears input field
 	$scope.clearInput = function(){
-		$scope.jobTitle = "";
-		$scope.jobCompany = "";
-		$scope.jobPlace = "";
-		$scope.jobBody = "";
-		$j("#dateFrom").datepicker( "setDate", null);
-		$j("#dateTill").datepicker( "setDate", null);
+		$j(".inputForm").children().each(function(){
+			$j(this).val("");
+		})
+		$j(".datePicker").datepicker( "setDate", null);
 	}
 
+	//switched to different sections
+	$scope.switchButton = function(object){
+		console.log(Object.keys($scope.jobs[0])[0])
+		$j(".inputForm").hide();
+		$j("#" + Object.keys(object[0])[0] + "-form" ).show();
+		$scope.clearInput()
+	}
 }]);
 
 //sets datepicker widgets format
 $j(function(){
-	$j( '#dateTill' ).datepicker({ dateFormat: 'dd-mm-yy'}); 
-	$j( '#dateFrom' ).datepicker({ dateFormat: 'dd-mm-yy'}); 
+	$j( '.datePicker' ).datepicker({ dateFormat: 'dd-mm-yy'}); 
 });
 
 
+[{
+	"education":"Webdeveloper",
+	"schoolName":"ROC",
+	"place":"Almere",
+	"state":"finished",
+	"dates":["27 januari 2016","04 februari 2016","Wed Jan 27 2016","Thu Feb 4 2016"],
+	"$$hashKey":"object:7"
+},
+{
+	"education":"Comp Science",
+	"schoolName":"HVA",
+	"place":"Almere",
+	"state":"finished",
+	"dates":["25 januari 2016","02 februari 2016","Mo Jan 25 2016","Tue Feb 2 2016"],
+	"$$hashKey":"object:8"
+},
+{
+	"education":""
+	,"schoolName":"",
+	"place":"",
+	"state":"",
+	"$$hashKey":"object:24"
+},
+{
+	"education":"1",
+	"schoolName":"1",
+	"place":"1",
+	"state":"1",
+	"dates":["25 december 2016","28 december 2016","Sun Dec 25 2016","Wed Dec 28 2016"]
+	,"$$hashKey":"object:26"
+},
+{
+	"education":"1",
+	"schoolName":"1",
+	"place":"1",
+	"state":"1",
+	"dates":["18 december 2016","29 december 2016","Sun Dec 18 2016","Thu Dec 29 2016"],
+	"$$hashKey":"object:28"
+},
+{
+	"education":"2",
+	"schoolName":"2",
+	"place":"2",
+	"state":"2",
+	"dates":["19 december 2016","26 december 2016","Mon Dec 19 2016","Mon Dec 26 2016"]
+}]
